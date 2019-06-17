2e82f3133ea09ee0fc47b6cdeb092abc63ec2d20
Thomas McNeela <thomas.mcneela@uscellular.com>
2019-06-17 11:57:12 -0500

    added set_confidence and get_ioc_details methods

--- mcneelat/pyutils/anomali.py
+++ mcneelat/pyutils/anomali.py
@@ -29,10 +29,19 @@ class ThreatStream(AbstractLogUtils):
         self.min_confidence = min_confidence
         self.last_modified_days = last_modified_days
         self.results_limit = results_limit
+        self.set_confidence(min_confidence)
+        AbstractLogUtils.__init__(self, verbose)
+
+    def set_confidence(self, min_confidence):
+        """
+        Set minimum confidence level.
+        :param min_confidence: minimum confidence level on a scale of 0 to 100
+        :return: None
+        """
+        self.min_confidence = min_confidence
         self.next_url_base = "%s?%s&status=active&confidence__gte=%i&limit=%i" % (
             self.intel_url_part, self.creds_url_part, self.min_confidence, self.results_limit
         )
-        AbstractLogUtils.__init__(self, verbose)
 
     def is_threat(self, test_object):
         """
@@ -40,17 +49,26 @@ class ThreatStream(AbstractLogUtils):
         :param test_object: indicator to check
         :return: True if malicious, False if not found active in ThreatStream
         """
+        result = self.get_ioc_details(test_object)
+        return result is not None
+
+    def get_ioc_details(self, test_object):
+        """
+        Get details for one specific IOC.
+        :param test_object: object to search for
+        :return: list of dictionaries (usually only one in the list) containing IOC details
+        """
         last_modified = (dt.today() - timedelta(days=self.last_modified_days)).strftime("%Y-%m-%dT00:00:00Z")
         next_url = "%s&modified_ts__gte=%s&value=%s" % (self.next_url_base, last_modified, test_object)
         try:
             json_data = requests.get(self.base_url + next_url).json()
         except ValueError:
-            return False
+            return None
         try:
             json_data.get('objects')[0]
         except:
-            return False
-        return True
+            return None
+        return json_data.get('objects')
 
     def get_iocs(self, icategory, severity="high"):
         """
@@ -63,9 +81,9 @@ class ThreatStream(AbstractLogUtils):
         next_url = "%s&modified_ts__gte=%s&meta.severity__contains=%s&itype=%s" % (
             self.next_url_base, last_modified, severity, ThreatStream.ICATEGORY_MAP[icategory]
         )
-        self.log("Starting to gather IOCs...")
+        self.log("[*] Starting to gather IOCs...")
         results = []
-        while next_url != None and next_url != "null":
+        while next_url is not None and next_url != "null":
             try:
                 json_data = requests.get(self.base_url + next_url).json()
             except ValueError:
